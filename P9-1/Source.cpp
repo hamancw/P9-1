a53c7df0238a77eccc9ea4fc05b16a963b80d54e
Cade Haman <c-haman@onu.edu>
2024-04-04 10:03:36 -0400

    Can now display every element in order of appearance in the array.

---
+++ P9-1/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime> 
+
+using namespace std;
+
+int main() {
+
+    srand(time(0));
+
+    int arr[10];
+    for (int i = 0; i < 10; ++i) {
+        arr[i] = rand() % 9000 + 1000;
+    }
+
+    cout << "The random integers: ";
+    for (int i = 0; i < 10; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
